8626c867aff8e2f531ab65be69c15d77fad30212
Wizelzeli <wizelzeli@yandex.ru>
2021-11-01 00:35:54 +0300

    Login / Registration post request, hashing

--- app/src/main/java/com/example/fafij/presentation/login/LoginActivity.java
+++ app/src/main/java/com/example/fafij/presentation/login/LoginActivity.java
@@ -6,9 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.fafij.R;
 import com.example.fafij.presentation.registration.RegistrationActivity;
+
+import org.json.JSONException;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 public class LoginActivity extends AppCompatActivity implements LoginContract.LoginViewInterface {
 
@@ -24,12 +30,39 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Lo
     /**
      * Отправляет в презентер данные из форм (int)
      */
-    public void sendFormInfo() {
+    public void sendFormInfo() throws JSONException, InvalidKeySpecException, NoSuchAlgorithmException {
         EditText login = (EditText)findViewById(R.id.login_edittext_login);
         EditText password = (EditText)findViewById(R.id.login_edittext_password);
         String loginString = login.getText().toString();
         String passwordString = password.getText().toString();
         presenter.onAuthorizationClick(loginString, passwordString);
+    }
+
+    @Override
+    public void testSuccessMessage(){
+        Toast.makeText(
+                this,
+                "Вы были успешно авторизированы.",
+                Toast.LENGTH_SHORT
+        ).show();
+    }
+
+    @Override
+    public void testFailMessage() {
+        Toast.makeText(
+                this,
+                "Произошла ошибка.",
+                Toast.LENGTH_SHORT
+        ).show();
+    }
+
+    @Override
+    public void testtest(String s) {
+        Toast.makeText(
+                this,
+                s,
+                Toast.LENGTH_SHORT
+        ).show();
     }
 
     /**
