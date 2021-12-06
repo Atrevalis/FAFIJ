721f3b41f02beea8be357445817cc66c196858b1
Wizelzeli <wizelzeli@yandex.ru>
2021-12-06 03:19:50 +0300

    Localhost url fix

--- app/src/main/java/com/example/fafij/models/Network/RetrofitApiClient.java
+++ app/src/main/java/com/example/fafij/models/Network/RetrofitApiClient.java
@@ -11,7 +11,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitApiClient {
-    public static String BASE_URL ="http://192.168.0.103:8081/";
+    public static String BASE_URL ="http://10.0.2.2:8081/";
     private static Retrofit retrofit;
     public static RetrofitApiInterface getClient(){
         if(retrofit == null){
